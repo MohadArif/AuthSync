ad428383335d1c2bf778f13782b3829a65d203aa
Md Arif <mohadarif9211@gmail.com>
2026-03-06 15:37:39 +0530

    adding the Asynchronous feature

---
+++ src/main/java/com/example/AuthSync/config/AsyncConfiguration.java
@@ -0,0 +1,12 @@
+package com.example.AuthSync.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ *  Enabling the Asynchronous
+ */
+@Configuration
+@EnableAsync
+public class AsyncConfiguration {
+}
